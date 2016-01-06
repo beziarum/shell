e0953fe12778dcd5a138ba6d090d53002e54cf4e
Raitei3 <antoine24.borde@gmail.com>
2016-01-06 21:59:20 +0100

    implémentation de date

--- Commandes_Internes.c
+++ Commandes_Internes.c
@@ -1,5 +1,12 @@
 #include "Commandes_Internes.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <time.h>
 
 void verifier(int b,char* m)
 {
@@ -21,4 +28,14 @@ int echo(){
   return 0;
 }
 
+int date(){
 
+  char c[256]; 
+  time_t tmp = time(NULL);
+  struct tm * t;
+  t =localtime(&tmp);
+
+  strftime(c, sizeof(c), "%A %d %B %Y, %X (UTC%z)",t); 
+  printf("%s\n", c);
+  return 0;
+}
