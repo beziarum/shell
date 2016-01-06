d87ac4e5ad13337175d2ff5b678caddd973e75f5
Raitei3 <antoine24.borde@gmail.com>
2016-01-06 19:45:30 +0100

    echo

--- Commandes_Internes.c
+++ Commandes_Internes.c
@@ -1,4 +1,18 @@
 #include "Commandes_Internes.h"
 
 
+int echo(){
+  void* ptr;
+  int c=1;
+  while (c>=1){
+    c=fread(&ptr,1,1,stdin);
+    fwrite(&ptr,1,1,stdout);
+    fprintf(stderr,"%d",c);
+  }
+  return 1;
+}
 
+int main (int argc, char** argv){
+  echo();
+  return EXIT_SCCUSS;
+}
