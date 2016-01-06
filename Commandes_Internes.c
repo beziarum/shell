0bad4f80ab015bef1365100c5ea8c8ae6b271c2a
Raitei3 <antoine24.borde@gmail.com>
2016-01-06 20:15:34 +0100

    implémentation de echo

--- Commandes_Internes.c
+++ Commandes_Internes.c
@@ -1,18 +1,24 @@
 #include "Commandes_Internes.h"
 
 
-int echo(){
-  void* ptr;
-  int c=1;
-  while (c>=1){
-    c=fread(&ptr,1,1,stdin);
-    fwrite(&ptr,1,1,stdout);
-    fprintf(stderr,"%d",c);
-  }
-  return 1;
+void verifier(int b,char* m)
+{
+    if(!b)
+    {
+	perror(m);
+	exit(EXIT_FAILURE);
+    }
 }
 
-int main (int argc, char** argv){
-  echo();
-  return EXIT_SCCUSS;
+int echo(){
+  char ptr;
+  int c=1;
+  while (c>=1){
+    c=fread(&ptr,1,c,stdin);
+    fwrite(&ptr,1,c,stdout);
+
+  }
+  return 0;
 }
+
+
