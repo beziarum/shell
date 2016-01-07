4b9382811fcd493b6f297ea4b4ed31f3d37e1585
paul <paul.beziau@laposte.net>
2016-01-07 10:59:17 +0100

    ajout des séquences conditionnelles de type « et »

--- Evaluation.c
+++ Evaluation.c
@@ -8,6 +8,7 @@
 int expr_simple(Expression* e, Contexte* c);
 int expr_bg(Expression* e, Contexte* c);
 int expr_sequence(Expression* e, Contexte* c);
+int expr_sequence_et(Expression* e, Contexte* c);
 
 typedef struct assoc {
     expr_t expr;
@@ -16,7 +17,8 @@ typedef struct assoc {
 
 assoc tab_expr[] = {{SIMPLE, expr_simple},
 		    {BG, expr_bg},
-		    {SEQUENCE, expr_sequence}};
+		    {SEQUENCE, expr_sequence},
+		    {SEQUENCE_ET, expr_sequence_et}};
 
 int expr_not_implemented (Expression* e, Contexte* c)
 {
@@ -60,6 +62,15 @@ int expr_simple (Expression* e, Contexte* c)
 	    return WIFEXITED(status) ? WEXITSTATUS(status) : WTERMSIG(status);
 	}
     }
+}
+
+int expr_sequence_et (Expression* e, Contexte* c)
+{
+    int ret=get_expr(e->gauche->type)(e->gauche, c);
+    if(ret != 0)
+	return ret;
+    else
+	return get_expr(e->droite->type)(e->droite, c);
 }
 
 int (*get_expr (expr_t expr)) (Expression*, Contexte*)
